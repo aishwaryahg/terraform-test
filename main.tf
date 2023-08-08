11e68709eb25fe7d4da1536d171bfa519943c17f
aishwaryahg <83292259+aishwaryahg@users.noreply.github.com>
2023-08-08 12:11:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,33 @@
-resource "google_storage_bucket" "bkt-dev" {
-  count = var.env=="dev" ? 3 : 3
-  name = "bktdev-gcpdev-tf00${count.index}"
-  location = var.region
+#create network and subnetwork
+resource "google_compute_network" "network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
 }
 
-resource "google_storage_bucket" "bkt-prod" {
-  count = var.env=="dev" ? 0 : 3
-  name = "bktprod-gcpprod-tf00${count.index}"
-  location = var.region
+ 
+
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = "my-subnetwork"
+  network       = google_compute_network.network.id
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+ 
+
+#create a virtual machine
+resource "google_compute_instance" "myinstance" {
+    name = "my-vm-instance"
+    machine_type =  "n1-standard-1"
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-11"
+        labels = {
+            my_label = "test-vm"
+        }
+      }
+    }
+    network_interface {
+      network = "my-network"
+      subnetwork = google_compute_subnetwork.subnetwork.self_link
+    }
 }
